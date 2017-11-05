a3899531f65da98f30e7252b8c3219d457ee982a
Jerin Lee <taurenjerin@qq.com>
2017-11-05 09:43:35 +0800

    fix FileHelper in Android.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

--- src/XP/XPlatform.Android/IO/FileHelper.cs
+++ src/XP/XPlatform.Android/IO/FileHelper.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 [assembly: Xamarin.Forms.Dependency( typeof( XPlatform.IO.FileHelper ) )]
 namespace XPlatform.IO {
   public class FileHelper : IFileHelper {
     public string GetLocalFilePath(string user_id, string filename) {
-      string path = Environment.GetFolderPath( Environment.SpecialFolder.Personal );
+      string path = System.Environment.GetFolderPath( System.Environment.SpecialFolder.Personal );
       return Path.Combine( path, user_id, filename );
     }
   }
